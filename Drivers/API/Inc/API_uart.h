cf51fbfc7ae3d33fa24583ed401449deacf8b27d
FranCDoc <fchiesadoc@gmail.com>
2024-08-13 00:59:24 -0300

    more

--- Drivers/API/Inc/API_uart.h
+++ Drivers/API/Inc/API_uart.h
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "stm32f4xx_hal.h"      /* <- HAL include */
+#include "stm32f4xx_hal.h" /* <- HAL include */
 
 /* Exported types ------------------------------------------------------------*/
 #ifndef BOOL_TYPE_DEFINITIOON_
@@ -16,29 +16,29 @@ typedef bool bool_t;
 #endif // BOOL_TYPE_DEFINITIOON_
 
 /* Exported constants --------------------------------------------------------*/
-#define MAXbUFFER                           256
-#define TxTIMEOUT                          5000
-#define RxTIMEOUT                            10
-#define USARTx                           USART3
-#define USARTx_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE();
-#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
-#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define MAXbUFFER 256
+#define TxTIMEOUT 5000
+#define RxTIMEOUT 10
+#define USARTx USART3
+#define USARTx_CLK_ENABLE() __HAL_RCC_USART3_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOD_CLK_ENABLE()
 
-#define USARTx_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
-#define USARTx_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+#define USARTx_FORCE_RESET() __HAL_RCC_USART3_FORCE_RESET()
+#define USARTx_RELEASE_RESET() __HAL_RCC_USART3_RELEASE_RESET()
 
 /* Definition for USARTx Pins */
-#define USARTx_TX_PIN                    GPIO_PIN_8
-#define USARTx_TX_GPIO_PORT              GPIOD
-#define USARTx_TX_AF                     GPIO_AF7_USART3
-#define USARTx_RX_PIN                    GPIO_PIN_9
-#define USARTx_RX_GPIO_PORT              GPIOD
-#define USARTx_RX_AF                     GPIO_AF7_USART3
+#define USARTx_TX_PIN GPIO_PIN_8
+#define USARTx_TX_GPIO_PORT GPIOD
+#define USARTx_TX_AF GPIO_AF7_USART3
+#define USARTx_RX_PIN GPIO_PIN_9
+#define USARTx_RX_GPIO_PORT GPIOD
+#define USARTx_RX_AF GPIO_AF7_USART3
 
 /* Exported functions ------------------------------------------------------- */
 bool_t uartInit(void);
-void   uartSendString(uint8_t * pstring);
-void   uartSendStringSize(uint8_t * pstring, uint16_t size);
-void   uartReceiveStringSize(uint8_t * pstring, uint16_t size);
+void uartSendString(uint8_t *pstring);
+void uartSendStringSize(uint8_t *pstring, uint16_t size);
+void uartReceiveStringSize(uint8_t *pstring, uint16_t size);
 int uartReceiveStringAndParseDate(uint8_t *pstring, uint16_t size, RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate);
 #endif /* API_INC_API_UART_H_ */
--- Drivers/API/Src/API_uart.c
+++ Drivers/API/Src/API_uart.c
@@ -12,43 +12,43 @@ static void Error_Handler(void);
 /* Public functions ----------------------------------------------------------*/
 
 /**
-  * @brief  Initialize UART with specific settings.
-  * @param  None.
-  * @retval bool_t: returns true if initialization is successful, otherwise false.
-  */
+ * @brief  Initialize UART with specific settings.
+ * @param  None.
+ * @retval bool_t: returns true if initialization is successful, otherwise false.
+ */
 
 bool_t uartInit(void)
 {
-  UartHandle.Instance        = USARTx;
-  UartHandle.Init.BaudRate   = 9600;
+  UartHandle.Instance = USARTx;
+  UartHandle.Init.BaudRate = 9600;
   UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
-  UartHandle.Init.StopBits   = UART_STOPBITS_1;
-  UartHandle.Init.Parity     = UART_PARITY_ODD;
-  UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
-  UartHandle.Init.Mode       = UART_MODE_TX_RX;
+  UartHandle.Init.StopBits = UART_STOPBITS_1;
+  UartHandle.Init.Parity = UART_PARITY_ODD;
+  UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  UartHandle.Init.Mode = UART_MODE_TX_RX;
   UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
 
   if (HAL_UART_Init(&UartHandle) != HAL_OK)
   {
     /* Initialization Error */
-   return false;
+    return false;
   }
 
-  uartSendString((uint8_t*)"UART init OK -> baud rate configured = 9600 | stopbits = 1 | parity = odd.\r\n");
+  uartSendString((uint8_t *)"UART init OK -> baud rate configured = 9600 | stopbits = 1 | parity = odd.\r\n");
 
   return true;
 }
 
 /**
-  * @brief  Send a null-terminated string via UART.
-  * @param  uint8_t * pstring: pointer to the null-terminated string.
-  * @retval None.
-  */
-void uartSendString(uint8_t * pstring)
+ * @brief  Send a null-terminated string via UART.
+ * @param  uint8_t * pstring: pointer to the null-terminated string.
+ * @retval None.
+ */
+void uartSendString(uint8_t *pstring)
 {
-  if(NULL != pstring && MAXbUFFER > strlen((char*)pstring))
+  if (NULL != pstring && MAXbUFFER > strlen((char *)pstring))
   {
-    if(HAL_OK != HAL_UART_Transmit(&UartHandle, pstring, strlen((char*)pstring), TxTIMEOUT))
+    if (HAL_OK != HAL_UART_Transmit(&UartHandle, pstring, strlen((char *)pstring), TxTIMEOUT))
       Error_Handler();
   }
   else
@@ -56,16 +56,16 @@ void uartSendString(uint8_t * pstring)
 }
 
 /**
-  * @brief  Send a specific number of characters from a null-terminated string via UART.
-  * @param  uint8_t * pstring: pointer to the null-terminated string.
-  * @param  uint16_t size: number of characters to send.
-  * @retval None.
-  */
-void uartSendStringSize(uint8_t * pstring, uint16_t size)
+ * @brief  Send a specific number of characters from a null-terminated string via UART.
+ * @param  uint8_t * pstring: pointer to the null-terminated string.
+ * @param  uint16_t size: number of characters to send.
+ * @retval None.
+ */
+void uartSendStringSize(uint8_t *pstring, uint16_t size)
 {
-  if(NULL != pstring && MAXbUFFER > size && 0 < size)
+  if (NULL != pstring && MAXbUFFER > size && 0 < size)
   {
-    if(HAL_OK != HAL_UART_Transmit(&UartHandle, pstring, size, TxTIMEOUT))
+    if (HAL_OK != HAL_UART_Transmit(&UartHandle, pstring, size, TxTIMEOUT))
       Error_Handler();
   }
   else
@@ -73,16 +73,16 @@ void uartSendStringSize(uint8_t * pstring, uint16_t size)
 }
 
 /**
-  * @brief  Receive a specific number of characters via UART.
-  * @param  uint8_t * pstring: pointer to the buffer where received characters will be stored.
-  * @param  uint16_t size: number of characters to receive.
-  * @retval None.
-  */
-void uartReceiveStringSize(uint8_t * pstring, uint16_t size)
+ * @brief  Receive a specific number of characters via UART.
+ * @param  uint8_t * pstring: pointer to the buffer where received characters will be stored.
+ * @param  uint16_t size: number of characters to receive.
+ * @retval None.
+ */
+void uartReceiveStringSize(uint8_t *pstring, uint16_t size)
 {
-  if(NULL != pstring && MAXbUFFER > size && 0 < size)
+  if (NULL != pstring && MAXbUFFER > size && 0 < size)
   {
-    if(HAL_ERROR == HAL_UART_Receive(&UartHandle, pstring, size, RxTIMEOUT))
+    if (HAL_ERROR == HAL_UART_Receive(&UartHandle, pstring, size, RxTIMEOUT))
       Error_Handler();
   }
   else
@@ -92,10 +92,10 @@ void uartReceiveStringSize(uint8_t * pstring, uint16_t size)
 /* Private API code ----------------------------------------------------------*/
 
 /**
-  * @brief  Handles errors by entering an infinite loop.
-  * @param  None.
-  * @retval None.
-  */
+ * @brief  Handles errors by entering an infinite loop.
+ * @param  None.
+ * @retval None.
+ */
 static void Error_Handler(void)
 {
   while (1)
